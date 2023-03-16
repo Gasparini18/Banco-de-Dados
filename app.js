b6069315578236ee1a1a528651f1cc25fbb625aa
Gasparini18 <112758450+Gasparini18@users.noreply.github.com>
2023-03-15 21:38:00 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const app = express()
 
+app.get("/", function(req,res){
+    res.sendFile(__dirname + "/view/index.html")
+})
+
 app.listen (8081, function(){
     console.log("Servidor Ativo!")
 })
